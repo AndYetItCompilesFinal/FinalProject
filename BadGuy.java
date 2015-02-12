fa17bf6c6e591ebe595c6f206a285043a1973a19
Zoe Baker <zcbaker@ymail.com>
2015-02-12 12:34:57 -0800

    Added Setters and Getters for Bad and GoodGuys

--- BadGuy.java
+++ BadGuy.java
@@ -27,6 +27,48 @@ public abstract class BadGuy
    public abstract int attack2();
    
    public abstract int attack3();
+   
+      
+   public int getMaxHp(){
+      return this.maxhp;
+   }
+   
+   public void setMaxHp(int val){
+      this.maxhp = val;
+   }
+   
+   public int getHp(){
+      return this.hp;
+   }
+   
+   public void setHp(int val){
+      this.hp = val;
+   }
+   
+   public int getStr(){
+      return this.str;
+   }
+   
+   public void setStr(int val){
+      this.str = val;
+   }
+   
+   public double getDef(){
+      return this.def;
+   }
+   
+   public void setDef(double val){ 
+      this.def = val;
+   }
+   
+   public int getSpd(){
+      return this.spd;
+   }
+   
+   public void setSpd(int val){
+      this.spd = val;
+   }
+    
 
 
 }
--- GoodGuy.java
+++ GoodGuy.java
@@ -1,7 +1,6 @@
 public abstract class GoodGuy
 {
    //Items items;
-   //Attack attack;
    int maxhp;
    int hp;
    int str;
@@ -27,5 +26,46 @@ public abstract class GoodGuy
    public abstract int attack2();
    
    public abstract int attack3();
+   
+   public int getMaxHp(){
+      return this.maxhp;
+   }
+   
+   public void setMaxHp(int val){
+      this.maxhp = val;
+   }
+   
+   public int getHp(){
+      return this.hp;
+   }
+   
+   public void setHp(int val){
+      this.hp = val;
+   }
+   
+   public int getStr(){
+      return this.str;
+   }
+   
+   public void setStr(int val){
+      this.str = val;
+   }
+   
+   public double getDef(){
+      return this.def;
+   }
+   
+   public void setDef(double val){ 
+      this.def = val;
+   }
+   
+   public int getSpd(){
+      return this.spd;
+   }
+   
+   public void setSpd(int val){
+      this.spd = val;
+   }
+    
 
 }//end of class
